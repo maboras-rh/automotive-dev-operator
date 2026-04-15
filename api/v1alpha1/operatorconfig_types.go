9932b94582208e9b897c66f718fd38e378b996ee
maboras-rh <253495051+maboras-rh@users.noreply.github.com>
2026-04-15 09:25:59 +0000

    chore: bump AIB image to 1.2.0

--- api/v1alpha1/operatorconfig_types.go
+++ api/v1alpha1/operatorconfig_types.go
@@ -25,7 +25,7 @@ import (
 
 const (
 	// DefaultAutomotiveImageBuilderImage is the default container image for automotive-image-builder
-	DefaultAutomotiveImageBuilderImage = "quay.io/centos-sig-automotive/automotive-image-builder:1.1.14"
+	DefaultAutomotiveImageBuilderImage = "quay.io/centos-sig-automotive/automotive-image-builder:1.2.0"
 
 	// DefaultYQHelperImage is the default yq helper image used in Tekton task steps
 	DefaultYQHelperImage = "quay.io/konflux-ci/yq:latest"
